58429cd72623a5fdb94dc2a77c49246a7aca113d
sync1313 <88001614+sync1313@users.noreply.github.com>
2021-07-28 12:35:28 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,19 @@
+function goForward(){
+    console.log("going forward");
+}
+
+function goReverse(){
+    console.log("going reverse"); 
+}
+
+function goRight(){
+    console.log("going right"); 
+}
+
+function goLeft(){
+    console.log("going left"); 
+}
+
+function allStop(){
+    console.log("everything stop"); 
+}
